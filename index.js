dee510977112a06a75af368f43145bba8ec25904
MD : Ridoy  Islam <162418372+programminsekbo@users.noreply.github.com>
2024-08-25 23:14:48 -0700

    Server Setup:

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello word")
+//1 ans  Server Setup:
+
+
+// Import the http module
+const http = require('http');
+
+// Define the port number
+const PORT = 5500;
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+// Make the server listen on the specified port
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
